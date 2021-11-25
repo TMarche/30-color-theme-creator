dcaac8170a08fe63ca91fa1c0261b704f84d4dc0
Troy Marchesseault <tmarchess@gmail.com>
2021-11-25 11:25:25 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import { Spacer } from "./components/genericStyled";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// What kind of styling do we want to be able to do?
+// What do we want it to apply to?
+// How should it be applied
+
+class App extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <header style={{ background: "red", height: "200px" }}>
+                    <h1 style={{ margin: "0 auto" }}>Color Theme Creator</h1>
+                </header>
+                <div
+                    style={{
+                        display: "flex",
+                        height: "calc(100vh - 200px)",
+                        background: "purple",
+                    }}
+                >
+                    <div style={{ flex: 1, background: "yellow" }}></div>
+                    <div style={{ flex: 1, background: "green" }}></div>
+                </div>
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
